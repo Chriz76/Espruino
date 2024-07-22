928b4b5946287dd447344dab021587ade318777e
Gonk <chris@zink.tv>
2024-07-22 19:33:38 +0200

    a

--- boards/BANGLEJS2.py
+++ boards/BANGLEJS2.py
@@ -24,7 +24,7 @@ info = {
 # 'default_console_tx' : "D6",
 # 'default_console_rx' : "D8",
 # 'default_console_baudrate' : "9600",
- 'variables' : 12000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 8000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
                       # Currently leaves around 38k of free stack - *loads* more than we need
  'io_buffer_size' : 512, # How big is the input buffer (in 4 byte words). Default on nRF52 is 256
  'bootloader' : 1,
