bd4209c07a1c4c8d9989fd750edb1be978e49c7f
Gonk <chris@zink.tv>
2024-07-22 22:27:39 +0200

    sadasd

--- libs/banglejs/jswrap_bangle.h
+++ libs/banglejs/jswrap_bangle.h
@@ -27,6 +27,12 @@ int jswrap_banglejs_isBacklightOn();
 void jswrap_banglejs_setLocked(bool isLocked);
 int jswrap_banglejs_isLocked();
 
+//void my_flush_cb(lv_display_t * display, const lv_area_t * area, uint8_t * px_map);
+
+//int getMilliseconds();
+void jswrap_banglejs_setup();
+void jswrap_banglejs_label();
+
 void jswrap_banglejs_setPollInterval(JsVarFloat interval);
 void jswrap_banglejs_setOptions(JsVar *options);
 JsVar *jswrap_banglejs_getOptions();
