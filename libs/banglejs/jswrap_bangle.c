600cb57f479d55a2bc0f4b297aa36edf37768faa
Gonk <chris@zink.tv>
2024-07-27 19:33:12 +0200

    sadasd

--- libs/banglejs/jswrap_bangle.c
+++ libs/banglejs/jswrap_bangle.c
@@ -3740,6 +3740,36 @@ void my_read_cb(lv_indev_t * indev, lv_indev_data_t*data)
 
 lv_indev_t * indev;
 
+static void value_changed_event_cb(lv_event_t * e);
+
+void lv_example_arc_1(void)
+{
+    lv_obj_t * label = lv_label_create(lv_screen_active());
+
+    /*Create an Arc*/
+    lv_obj_t * arc = lv_arc_create(lv_screen_active());
+    lv_obj_set_size(arc, 150, 150);
+    lv_arc_set_rotation(arc, 135);
+    lv_arc_set_bg_angles(arc, 0, 270);
+    lv_arc_set_value(arc, 10);
+    lv_obj_center(arc);
+    lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
+
+    /*Manually update the label for the first time*/
+    lv_obj_send_event(arc, LV_EVENT_VALUE_CHANGED, NULL);
+}
+
+static void value_changed_event_cb(lv_event_t * e)
+{
+    lv_obj_t * arc = lv_event_get_target(e);
+    lv_obj_t * label = lv_event_get_user_data(e);
+
+    lv_label_set_text_fmt(label, "%" LV_PRId32 "%%", lv_arc_get_value(arc));
+
+    /*Rotate the label to the current position of the arc*/
+    lv_arc_rotate_obj_to_angle(arc, label, 25);
+}
+
 /*JSON{
     "type" : "staticmethod",
     "class" : "Bangle",
@@ -3764,6 +3794,8 @@ void jswrap_banglejs_lvgl(int step) {
 	else if (step == 3)
 		lv_tick_set_cb(getMilliseconds);  
 	else if (step == 4) {
+		lv_example_arc_1();	
+		/*
 		jsiConsolePrintf("A %d", jshGetSystemTime() / jshGetTimeFromMilliseconds(1000));
 		jsiConsolePrintf("B %d", getMilliseconds());
 		// Create a simple LVGL object to test
@@ -3771,6 +3803,7 @@ void jswrap_banglejs_lvgl(int step) {
 		lv_label_set_text(label, "Hello");
 		lv_obj_align(label, LV_ALIGN_CENTER, labelPos, labelPos);	
 		labelPos += 10;
+		*/
 	} else if (step == 5)
 		timerHandler1 = true;
 	else if (step == 6)
