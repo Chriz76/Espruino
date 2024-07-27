2ca7289950ce8cc89549ac47223b6fd489233b57
Gonk <chris@zink.tv>
2024-07-27 20:26:42 +0200

    asdsada

--- libs/banglejs/jswrap_bangle.c
+++ libs/banglejs/jswrap_bangle.c
@@ -3740,13 +3740,13 @@ void my_read_cb(lv_indev_t * indev, lv_indev_data_t*data)
 
 lv_indev_t * indev;
 
+/*
 static void value_changed_event_cb(lv_event_t * e);
 
 void lv_example_arc_1(void)
 {
     lv_obj_t * label = lv_label_create(lv_screen_active());
 
-    /*Create an Arc*/
     lv_obj_t * arc = lv_arc_create(lv_screen_active());
     lv_obj_set_size(arc, 150, 150);
     lv_arc_set_rotation(arc, 135);
@@ -3755,7 +3755,6 @@ void lv_example_arc_1(void)
     lv_obj_center(arc);
     lv_obj_add_event_cb(arc, value_changed_event_cb, LV_EVENT_VALUE_CHANGED, label);
 
-    /*Manually update the label for the first time*/
     lv_obj_send_event(arc, LV_EVENT_VALUE_CHANGED, NULL);
 }
 
@@ -3766,9 +3765,9 @@ static void value_changed_event_cb(lv_event_t * e)
 
     lv_label_set_text_fmt(label, "%" LV_PRId32 "%%", lv_arc_get_value(arc));
 
-    /*Rotate the label to the current position of the arc*/
     lv_arc_rotate_obj_to_angle(arc, label, 25);
 }
+*/
 
 /*JSON{
     "type" : "staticmethod",
